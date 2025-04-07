258ae8df141b684b811167041ec81c1c6a09df7f
sshree <sshrijan65@gmail.com>
2025-04-07 10:51:45 +0545

    hashTable created

---
+++ hashTable.java
@@ -0,0 +1,19 @@
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+public class hashTable{
+    // This is a simple example of how to use a hashtable in Java
+    public static void main(String[] args) {
+        // Create a hashtable
+        Hashtable<String , String> ht = new Hashtable<>();
+        ht.put("key 1","Romeo");
+        ht.put("key 2","Peter");
+        
+        Enumeration<String> name = ht.keys();
+        // Print the keys and values in the hashtable
+        while(name.hasMoreElements()){
+            String key = name.nextElement();
+            System.out.println("Key:" + key + " \t Value :" + ht.get(key));
+        }
+    }
+}
