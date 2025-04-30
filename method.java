a2f3002907837b472c7a720ff68db0fe7e7ecc6c
sshree <sshrijan65@gmail.com>
2025-04-30 15:27:51 +0545

    Merge remote-tracking branch 'origin/main'

--- method.java
+++ method.java
@@ -1,28 +1,27 @@
 public class method {
     public static void main(String[] args) {
-        method m = new method();
-         int result = m.sum(5);
+        int result = sum(5);
         System.out.println(result);
     }
-public int sum(int x) {
-    if (x > 0) {
-        return x + sum(x - 1);
-    } else {
-        return 0;
+    public static int sum(int x) {
+        if (x > 0) {
+            return x + sum(x - 1);
+        } else {
+            return 0;
+        }
     }
-}
 }
 //public class method {
 //    public static void main(String[] args) {
-//        int result = sum(5);
+//        method m = new method();
+//         int result = m.sum(5);
 //        System.out.println(result);
 //    }
-//
-//    public static int sum(int x) {
-//        if (x > 0) {
-//            return x + sum(x - 1);
-//        } else {
-//            return 0;
-//        }
+//public int sum(int x) {
+//    if (x > 0) {
+//        return x + sum(x - 1);
+//    } else {
+//        return 0;
 //    }
 //}
+}
